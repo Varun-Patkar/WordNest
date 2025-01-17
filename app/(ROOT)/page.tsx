e4a4d5a4be5da7290c96b8e24a98e65e8ec7897c
Varun Anand Patkar <varunpatkar501@gmail.com>
2025-01-17 23:30:23 +0530

    Added Hero and Navbar completely

--- app/(ROOT)/page.tsx
+++ app/(ROOT)/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
 	return (
 		<>
-			<h1 className="text-2xl">HOME</h1>
+			<section className="pink_container">
+				<h1 className="heading">
+					Write, Share, Inspire,
+					<br /> Read, Explore, Connect
+				</h1>
+				<p className="sub-heading !max-2-3xl">
+					Write Blogs, Share Stories, and Connect with a World of Readers.
+				</p>
+				<SearchForm />
+			</section>
 		</>
 	);
 }
