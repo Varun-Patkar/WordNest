784c26ee943906b31a709981f2e8ea81cbaff035
Varun Anand Patkar <varunpatkar501@gmail.com>
2025-01-24 20:58:52 +0530

    Added post page

---
+++ app/(ROOT)/post/[id]/page.tsx
@@ -0,0 +1,72 @@
+import { formatDate } from "@/lib/utils";
+import { client } from "@/sanity/lib/client";
+import { POST_BY_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React from "react";
+import markdownit from "markdown-it";
+
+const md = markdownit();
+export const experimental_ppr = true;
+
+const Post = async ({ params }: { params: Promise<{ id: string }> }) => {
+	const id = (await params).id;
+	const post = await client.fetch(POST_BY_ID_QUERY, { id });
+	if (!post) return notFound();
+	const parsedContent = md.render(post?.blogText);
+	return (
+		<>
+			<section className="pink_container !min-h-[230px]">
+				<p className="tag">{formatDate(post._createdAt)}</p>
+				<h1 className="heading">{post.title}</h1>
+				<p className="sub-heading !max-w-5xl">{post.description}</p>
+			</section>
+			<section className="section_container">
+				<img
+					src={post.image}
+					alt="thumbnail"
+					className="w-full h-auto rounded-xl"
+				></img>
+				<div className="space-y-5 mt-10 max-w-4xl mx-auto">
+					<div className="flex-between gap-5">
+						<Link
+							href={`/user/${post.author?._id}`}
+							className="flex gap-2 items-center mb-3"
+						>
+							<Image
+								src={post.author.image}
+								alt="avatar"
+								width={64}
+								height={64}
+								className="rounded-full drop-shadow-lg"
+							/>
+							<div>
+								<p className="text-20-medium">{post.author.name}</p>
+								<p className="text-16-medium !text-black-300">
+									@{post.author.username}
+								</p>
+							</div>
+						</Link>
+						<Link href={`/?query=${post.category?.toLowerCase()}`}>
+							<p className="category-tag">{post.category}</p>
+						</Link>
+					</div>
+					<h3 className="text-30-bold">Post Details</h3>
+					{parsedContent ? (
+						<article
+							className="prose max-w-4xl font-work-sans break-all"
+							dangerouslySetInnerHTML={{ __html: parsedContent }}
+						></article>
+					) : (
+						<p className="no-result"></p>
+					)}
+				</div>
+				<hr className="divider" />
+				{/*TODO: recommended posts here*/}
+			</section>
+		</>
+	);
+};
+
+export default Post;
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -13,3 +13,19 @@ export const POSTS_QUERY = defineQuery(`*[_type=="post" && defined(slug.current)
     category,
     image
 }`)
+
+export const POST_BY_ID_QUERY =
+  defineQuery(`*[_type == "post" && _id == $id][0]{
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, username, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+  blogText
+}`);
